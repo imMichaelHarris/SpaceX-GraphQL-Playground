04476c9e1992c161008db852c643e21931cfa328
Michael Harris <mhguitar1997@gmail.com>
2020-08-15 23:10:12 -0700

    start

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,14 @@
 require('dotenv').config();
+
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const { createStore } = require('./utils');
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
